d4999d20dcaae0050140dae9611d0d389d8d9fbf
Jakob Borg <jakob@kastelo.net>
2023-12-14 09:48:22 +0100

    chore: Cleaner service separation, event handling

--- cmd/syncthing-autoacceptd/main.go
+++ cmd/syncthing-autoacceptd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"errors"
+	"fmt"
 	"log/slog"
 	"os"
 	"runtime"
@@ -16,9 +16,8 @@ import (
 	"kastelo.dev/syncthing-autoacceptd/internal/api"
 	"kastelo.dev/syncthing-autoacceptd/internal/build"
 	"kastelo.dev/syncthing-autoacceptd/internal/config"
+	"kastelo.dev/syncthing-autoacceptd/internal/processor"
 )
-
-var errMalformedEvent = errors.New("malformed event")
 
 type CLI struct {
 	Address  string `short:"a" help:"Address of Syncthing API" default:"127.0.0.1:8384" env:"SYNCTHING_AUTOACCEPTD_ADDRESS"`
@@ -47,85 +46,53 @@ func main() {
 
 	l.Info("Starting Syncthing Auto Accept Daemon", "version", build.GitVersion, "os", runtime.GOOS, "arch", runtime.GOARCH)
 
-	main := suture.NewSimple("main")
-	main.ServeBackground(context.Background())
-
-	// Read and validate config file
-	var patterns config.Configuration
-	bs, err := os.ReadFile(cli.Patterns)
+	config, err := loadConfig(cli.Patterns)
 	if err != nil {
-		l.Error("Failed to read config", "error", err)
-		os.Exit(2)
-	}
-	if err := patterns.Validate(); err != nil {
-		l.Error("Failed to validate config", "error", err)
-		os.Exit(2)
+		l.Error("Failed to load config", "error", err)
+		os.Exit(1)
 	}
 
-	uo := prototext.UnmarshalOptions{}
-	if err := uo.Unmarshal(bs, &patterns); err != nil {
-		l.Error("Failed to unmarshal config", "error", err)
-		os.Exit(2)
-	}
-
+	api := api.NewAPI(l, cli.Address, cli.APIKey)
 	types := []events.EventType{events.ConfigSaved, events.DeviceRejected, events.FolderRejected}
-	st := api.NewAPI(l, cli.Address, cli.APIKey, types)
-	main.Add(st)
+	proc := processor.NewEventListener(l, api, config, types)
 
-	ver, err := st.GetSystemVersion()
-	if err != nil {
-		l.Error("Failed to get Syncthing version", "error", err)
-		os.Exit(1)
-	}
-	stat, err := st.GetSystemStatus()
-	if err != nil {
-		l.Error("Failed to get Syncthing status", "error", err)
-		os.Exit(1)
-	}
-	l.Info("Connected to Syncthing", "version", ver.Version, "os", ver.OS, "arch", ver.Arch, "id", stat.MyID)
-
-	l.Debug("Listening for events...", "types", types)
-
-	for ev := range st.Events() {
-		l.Debug("Received event", "type", ev.Type, "id", ev.GlobalID)
-		switch ev.Type {
-		case events.DeviceRejected:
-			data, err := getDeviceRejectedData(ev)
-			if err != nil {
-				l.Error("Failed to process DeviceRejected event", "error", err)
-				continue
+	main := suture.New("main", suture.Spec{
+		FailureThreshold: 2,
+		EventHook: func(e suture.Event) {
+			if e.Type() == suture.EventTypeResume {
+				l.Info("Service resuming", "event", e.String())
+			} else if v, ok := e.Map()["restarting"].(bool); ok && v {
+				l.Warn("Service restarting", "event", e.String())
+			} else {
+				l.Error("Service error", "event", e.String())
 			}
-			log := slog.With("device", data.device, "name", data.name, "address", data.address)
-			if err := handleDeviceRejected(log, st, data, &patterns); err != nil {
-				l.Error("Failed to process device", "error", err)
-			}
-		case events.FolderRejected:
-			l.Debug("FolderRejected event ignored", "event", ev)
-		}
+		},
+	})
+
+	main.Add(api)
+	main.Add(proc)
+
+	if err := main.Serve(context.Background()); err != nil {
+		l.Error("Failed to run service", "error", err)
+		os.Exit(1)
 	}
 }
 
-func handleDeviceRejected(l *slog.Logger, st *api.API, data *deviceRejectedData, cfg *config.Configuration) error {
-	addDevice, addFolders, error := getDeviceRejectedConfigs(data, cfg)
-	if error != nil {
-		if errors.Is(error, errNoMatchingPattern) {
-			l.Info("No matching pattern found")
-			return nil
-		}
-		return error
+func loadConfig(path string) (*config.Configuration, error) {
+	bs, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("reading config: %w", err)
 	}
 
-	l.Info("Accepting device")
-	if err := st.SetDevice(addDevice); err != nil {
-		l.Error("Failed to add device", "error", err)
-	}
-	for _, fld := range addFolders {
-		l := l.With("folder", fld.ID)
-		l.Info("Accepting folder")
-		if err := st.SetFolder(fld); err != nil {
-			l.Error("Failed to add folder", "error", err)
-		}
+	uo := prototext.UnmarshalOptions{}
+	var patterns config.Configuration
+	if err := uo.Unmarshal(bs, &patterns); err != nil {
+		return nil, fmt.Errorf("parsing config: %w", err)
 	}
 
-	return nil
+	if err := patterns.Validate(); err != nil {
+		return nil, fmt.Errorf("validating config: %w", err)
+	}
+
+	return &patterns, nil
 }
